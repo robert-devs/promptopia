467420308d3c72f37e4b17d23bbb570adf97efd8
robert <mainrobert04@gmail.com>
2023-05-18 13:06:24 +0300

    adding the prompt models

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -4,40 +4,46 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
-const CreatePrompt = ()=>{
-    const[submitting,setSubmitting] = useState(false)
-    const [post,setPost] =useState({
-        prompt:"",
-        tag:""
-    })
+const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
 
-    const createPrompt =async (e) =>{
-        e.preventDefault()
-        setSubmitting(true)
+  const [submitting, setIsSubmitting] = useState(false);
+  const [post, setPost] = useState({ prompt: "", tag: "" });
 
-        try {
-            const res = await fetch('/api/prompt/new',{
-                method:"POST",
-                body:JSON.stringify({
-                    prompt:post.prompt,
-                    userId:session?.user.id,
-                    tag:post.tag
-                })
-            })
-        } catch (error) {
-            
-        }
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
 
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
     }
+  };
 
-    return(
-            <Form
-                type="Create"
-                post={post}
-                setPost={setPost}
-                submitting = {submitting}
-                handleSubmit = {createPrompt}
-            />
-    )
-}
-export default CreatePrompt
+  return (
+    <Form
+      type='Create'
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
+
+export default CreatePrompt;
