21fc2ce138f8d9715a3ddb7633ab1bc97680f059
robert <mainrobert04@gmail.com>
2023-05-22 13:32:58 +0300

    updates on profile and page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,33 +1,39 @@
-"use client";
 import Profile from "@components/Profile";
-import React, { useState } from "react";
-import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
 const MyProfile = () => {
-    const {data:session} = useSession()
-    const [posts ,setPosts] = useState()
-    const handleDelete = async () => {}
-    const handleEdit = () => {}
+  const { data: session } = useSession();
+  const [posts, setPosts] = useState();
+  const handleDelete = async () => {};
+  const handleEdit = () => {};
 
-    useEffect(()=>{
-      const fetchPosts = async() =>{
-        const res = await fetch(`/api/users/${session?.user.id}/posts`)
-        const data = await res.json()
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await res.json();
 
-        setPosts(data)
-      }
-      if(session?.user.id)fetchPosts()
-    },[])
+      setPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, []);
+
   return (
-    <Profile 
-        name = "my"
-        desc ="welcome to my personalized profile"
-        data ={posts}
-        handleDelete ={handleDelete}
-        handleEdit ={handleEdit}
+    <Profile
+      name="my"
+      desc="welcome to my personalized profile"
+      data={posts}
+      handleDelete={handleDelete}
+      handleEdit={handleEdit}
     />
-  )
-}
+  );
+};
 
-export default MyProfile
+const MyProfileWithSession = () => (
+  <SessionProvider>
+    <MyProfile />
+  </SessionProvider>
+);
+
+export default MyProfileWithSession;
